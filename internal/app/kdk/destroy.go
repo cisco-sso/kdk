96992f89c554317a7d506c8eaa4499ae7dce6761
David C Wang <dcwangmit01@gmail.com>
2018-08-24 22:48:59 +0000

    Replace promptui with custom simpleprompt for Win10
    * Use a simple custom prompter because
      all of the opensource ones are fancy
      and don't work on Win10 git bash shell

--- internal/app/kdk/destroy.go
+++ internal/app/kdk/destroy.go
@@ -18,9 +18,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/Sirupsen/logrus"
+	"github.com/cisco-sso/kdk/internal/pkg/utils/simpleprompt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-	"github.com/manifoldco/promptui"
 )
 
 func Destroy(ctx context.Context, dockerClient *client.Client, logger logrus.Entry) error {
@@ -44,11 +44,12 @@ func Destroy(ctx context.Context, dockerClient *client.Client, logger logrus.Ent
 		logger.Info("Destroying KDK container(s)...")
 		for _, containerId := range containerIds {
 			fmt.Printf("Delete KDK container [%s][%v]\n", KdkConfig.AppConfig.Name, containerId[:8])
-			prompt := promptui.Prompt{
-				Label:     "Continue",
-				IsConfirm: true,
+			prompt := simpleprompt.Prompt{
+				Text:     "Continue? [y/n] ",
+				Loop:     true,
+				Validate: simpleprompt.ValidateYorN,
 			}
-			if _, err := prompt.Run(); err != nil {
+			if result, err := prompt.Run(); err != nil || result == "n" {
 				logger.Error("KDK container deletion canceled or invalid input.")
 				return nil
 			}
---
+++ internal/pkg/utils/simpleprompt/simple_prompt.go
@@ -0,0 +1,73 @@
+package simpleprompt
+
+/* Usage
+sp := Prompt{
+	Text: "Mount your /keybase directory within KDK? [y/n] ",
+	Loop: true,
+	Validate: ValidateYorN,
+}
+
+result, err := sp.Run()
+fmt.Printf("result %v, err %v\n", result, err)
+*/
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Prompt struct {
+	Text     string
+	Loop     bool
+	Validate func(string) error
+}
+
+func (sp *Prompt) Run() (string, error) {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		// Print the description
+		fmt.Print(sp.Text)
+
+		// Block and read the input
+		scanner.Scan()
+		text := scanner.Text()
+
+		// If no validation function exists, return the text immediately
+		if sp.Validate == nil {
+			return text, nil
+		}
+
+		// Otherwise, run the validation function
+		if err := sp.Validate(text); err == nil {
+			return text, nil
+		} else {
+			// If the function didn't validate, print why, and continue the loop
+			fmt.Println(err)
+		}
+
+		// If we are not looping, break after the first iteration
+		if sp.Loop == false {
+			break
+		}
+	}
+	return "", errors.New("Failed to capture valid input")
+}
+
+func ValidateYorN(input string) error {
+	if input == "y" || input == "n" {
+		return nil
+	}
+	return errors.New("Input must be 'y' or 'n'")
+}
+
+func ValidateDirExists(input string) error {
+
+	if _, err := os.Stat(input); err == nil {
+		return nil
+	}
+	return errors.New("Input directory must exist")
+}
