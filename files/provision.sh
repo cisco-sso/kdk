f7d9bcbc59a2ad9a3815e554336e7f9b53ca4064
Ryan Luckie <rluckie@cisco.com>
2019-12-13 01:01:40 -0600

    git fixes

--- files/provision.sh
+++ files/provision.sh
@@ -452,9 +452,7 @@ function layer_build_apps_not_provided_by_os_packages() {
             --enable-multibyte \
             --enable-rubyinterp=yes \
             --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/ \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-cscope \
