33673e976577f0cba08a0ddc8967094627cb11a7
Hang Xie <7977860+hangxie@users.noreply.github.com>
2020-02-28 11:04:00 -0800

    pre-install jb v0.3.0

--- files/provision.sh
+++ files/provision.sh
@@ -415,6 +415,7 @@ function layer_go_get_installs() {
 	/usr/local/go/bin/go build -o /go/bin/mh && \
 	ln -sf /go/bin/mh /go/bin/multihelm
     GO111MODULE=on /usr/local/go/bin/go get github.com/mikefarah/yq/v2
+    GO111MODULE=on /usr/local/go/bin/go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb@v0.3.0
     rm -rf /root/.cache/go-build
     rm -rf /go/src
 }
